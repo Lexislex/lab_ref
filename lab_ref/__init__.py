f439e45607b8a7aa43a991d8c2711892e1c18163
Alexey Alepko <alexey.alepko@gmail.com>
2025-09-04 15:23:34 +0300

    docs: улучшить документацию и добавить статус Alpha
    - Полностью переработан README.md с красивым оформлением
    - Добавлены эмодзи, значки статуса и подробные примеры
    - Указан Alpha статус проекта с предупреждениями
    - Обновлена версия до 0.1.0a1 (alpha release)
    - Упрощен __init__.py под текущую функциональность
    - Исправлен .gitignore для Python проектов
    - Создан новый дистрибутив с alpha версией

--- lab_ref/__init__.py
+++ lab_ref/__init__.py
@@ -9,7 +9,8 @@
 См. docstrings у функций для подробной справки.
 """
 
-# Функциональный API (основной для простых случаев)
+__version__ = "0.1.0a1"
+
 from .utils import (
     validate_references_structure,
     load_references,
@@ -20,41 +21,9 @@ from .utils import (
     print_test_names_report,
     get_test_keys,
     print_test_types_report,
-    # Новые функции для работы с биоматериалами
-    list_biomaterials,
-    get_biomaterial_reference,
-    check_biomaterial_value,
-    load_lab_studies,
-    list_lab_studies,
-    get_study_info,
-    get_study_tests,
-    get_study_biomaterials,
-    get_preferred_biomaterial,
-    check_study_values,
-    print_biomaterials_report,
-    print_lab_studies_report,
-    # Функции для работы с пользовательскими папками референсов
-    set_references_dir,
-    get_current_references_dir,
-    reset_references_dir,
-    copy_references_template,
-)
-
-# ООП API (для сложных сценариев и удобства)
-from .models import (
-    AgeRange,
-    Reference,
-    Test,
-    ReferenceManager,
-    LabResult,
-    # Новые классы для работы с биоматериалами
-    BiomaterialManager,
-    LabStudyManager,
-    StudyResult,
 )
 
 __all__ = [
-    # Функциональный API
     "validate_references_structure",
     "load_references",
     "list_test_types",
@@ -64,34 +33,6 @@ __all__ = [
     "print_test_names_report",
     "get_test_keys",
     "print_test_types_report",
-    # Новые функции для работы с биоматериалами
-    "list_biomaterials",
-    "get_biomaterial_reference",
-    "check_biomaterial_value",
-    "load_lab_studies",
-    "list_lab_studies",
-    "get_study_info",
-    "get_study_tests",
-    "get_study_biomaterials",
-    "get_preferred_biomaterial",
-    "check_study_values",
-    "print_biomaterials_report",
-    "print_lab_studies_report",
-    # Функции для работы с пользовательскими папками референсов
-    "set_references_dir",
-    "get_current_references_dir",
-    "reset_references_dir",
-    "copy_references_template",
-    # ООП API
-    "AgeRange",
-    "Reference",
-    "Test",
-    "ReferenceManager",
-    "LabResult",
-    # Новые классы для работы с биоматериалами
-    "BiomaterialManager",
-    "LabStudyManager",
-    "StudyResult",
 ]
 
 # Помечаем ре-экспортированные функции как принадлежащие модулю `lab_ref`,
@@ -103,5 +44,4 @@ for _name in __all__:
             _obj.__module__ = __name__
         except Exception:
             pass
-
 
